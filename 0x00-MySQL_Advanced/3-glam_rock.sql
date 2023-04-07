461eccced46769d15fbcfbf3edb7a09b5639a6c8
cappochinho <edwardprempeh18@gmail.com>
2023-04-07 17:18:07 +0100

    Task 4

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Lists all bands with Glam rock as main style
 
-SELECT band_name, (IFNULL(split, formed) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;

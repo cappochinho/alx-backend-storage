d5130d3c8920d6e5a76f99935bdecb371f3ee8cb
cappochinho <edwardprempeh18@gmail.com>
2023-04-13 00:01:52 +0100

    Task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+"""
+this module contains the function update_topics
+"""
+
+
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name
+    """
+
+    return mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}}).modified_count
